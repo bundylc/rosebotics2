c0d59f90b23dca8af4670f040646215983f8f769
ritenojb <ritenojb@rose-hulman.edu>
2018-11-12 16:32:26 -0500

    Added test for the beacon buttons

--- src/JacobRitenour.py
+++ src/JacobRitenour.py
@@ -10,9 +10,9 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
-    test_color_sensor()
+    #test_color_sensor()
     #test_proximity_sensor()
-
+    test_beacon_buttons()
 
 def test_color_sensor():
     """Test for the robot's color sensor"""
@@ -30,5 +30,19 @@ def test_proximity_sensor():
         if (robot.proximity_sensor.get_distance_to_nearest_object_in_inches() < 15):
             ev3.Sound.beep().wait()
 
+def test_beacon_buttons():
+    """Test for the robot responding to the beacon buttons"""
+    robot = rb.Snatch3rRobot()
+    while (True):
+        if robot.beacon_button_sensor.is_top_red_button_pressed()==True:
+            robot.drive_system.start_moving(50,50)
 
+        if robot.beacon_button_sensor.is_bottom_red_button_pressed()==True:
+            ev3.Sound.speak('hello')
+
+        if robot.beacon_button_sensor.is_top_blue_button_pressed()==True:
+            robot.arm.calibrate()
+
+        if robot.beacon_button_sensor.is_bottom_blue_button_pressed()==True:
+            robot.drive_system.stop_moving()
 main()
