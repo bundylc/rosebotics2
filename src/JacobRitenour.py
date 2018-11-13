1a7fc711d4fb232ce5afd420bf1c156d86c8fc8f
ritenojb <ritenojb@rose-hulman.edu>
2018-11-12 23:03:28 -0500

    Finished methods for color sensor and beacon button sensor

--- src/JacobRitenour.py
+++ src/JacobRitenour.py
@@ -10,15 +10,16 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
-    #test_color_sensor()
+    test_color_sensor()
     #test_proximity_sensor()
-    test_beacon_buttons()
+    #test_beacon_buttons()
+    #doctor_who_bot()
 
 def test_color_sensor():
     """Test for the robot's color sensor"""
     robot=rb.Snatch3rRobot()
     robot.drive_system.start_moving(20,20)
-    robot.color_sensor.wait_until_color_is(2)
+    robot.color_sensor.wait_until_color_is(5)
     robot.drive_system.stop_moving()
 
 
@@ -38,11 +39,22 @@ def test_beacon_buttons():
             robot.drive_system.start_moving(50,50)
 
         if robot.beacon_button_sensor.is_bottom_red_button_pressed()==True:
-            ev3.Sound.speak('hello')
+            ev3.Sound.speak('Hello, I am the Doctor')
 
         if robot.beacon_button_sensor.is_top_blue_button_pressed()==True:
-            robot.arm.calibrate()
+            robot.drive_system.start_moving(-50,-50)
 
         if robot.beacon_button_sensor.is_bottom_blue_button_pressed()==True:
             robot.drive_system.stop_moving()
+
+def doctor_who_bot():
+    robot = rb.Snatch3rRobot()
+    robot.arm.raise_arm_and_close_claw()
+    ev3.Sound.speak('Exterminate')
+    print(robot.camera.get_biggest_blob().get_area())
+    robot.drive_system.start_moving(15,15)
+    while True:
+        if robot.camera.get_biggest_blob().get_area()>300:
+            robot.drive_system.stop_moving()
+
 main()
